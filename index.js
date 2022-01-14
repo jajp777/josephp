d75145837eeb55b43ef9ef89f7b3c0551625dec8
jajp777 <jajp777@gmail.com>
2022-01-14 17:45:44 +0530

    Improved code

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
-console.log(`Joseph Pradeep 
+const clearConsole = require('clear-any-console');
+ 
+// Clears the console.
+clearConsole();
+console.log(`Joseph Pradeep  hello1
 
 Award-winning Open-source enginner and developer.
 
