e5b7496b13322b272536e002e038de82e6f3d975
jajp777 <jajp777@gmail.com>
2022-01-17 15:07:52 +0530

    second

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 const clearConsole = require('clear-any-console');
- 
+const pkgJSON = require("./package.json");
+const welcome = require('cli-welcome');
 // Clears the console.
 clearConsole();
-console.log(`Joseph Pradeep  hello1
+// console.log(`
+// NAME: ${pkgJSON.name}
+// VERSION: ${pkgJSON.version}
+// DESCRIPTION: ${pkgJSON.description}
+// `)
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Howdy, nice to meet ya!`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true,
+})
+console.log(`Joseph Pradeep  hello3
 
 Award-winning Open-source enginner and developer.
 
